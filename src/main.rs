8e4ddd0555cf51974ea98711520a32c1d9b74914
Stas <jinfar@ymail.com>
2021-03-20 21:26:20 +0500

    ubral method new

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,12 @@ mod shar;
 
 fn main() {
     println!("Hello, world!");
-    // shar::run();
-    let w = shar::generate(3);
-    println!("{:?}", w[1].massa);
+    shar::run();
+    // let mut w = shar::generate(3);
+    // println!("{:?}", w[1].show());
+    // let mut tmp = &w[1];
+    // let mut tmp2 = &w[2];
+    // w[1].pull_by(&w[2]);
+    // w[1].pull_by(tmp2);
+    // println!("{:?}", w[1].massa);
 }
--- src/shar.rs
+++ src/shar.rs
@@ -1,4 +1,4 @@
-
+#![allow(dead_code)]
 
 pub struct Shar {
     pub massa: f64,
@@ -11,20 +11,8 @@ pub struct Shar {
 }
 
 impl Shar{
-    
-    fn new(massa: f64, x: f64, y: f64, velx: f64, vely: f64, accx: f64, accy: f64) -> Shar{
-        Shar{
-            massa: massa,
-            x: x,
-            y: y,
-            velx: velx,
-            vely: vely,
-            accx: accx,
-            accy: accy,
-        }
-    }
-
-    fn pull_by(&mut self, other: &Shar){
+        
+    pub fn pull_by(&mut self, other: &Shar){
         let g = 1.0;
         let dist = ((self.x - other.x).powi(2)+(self.y - other.y).powi(2)).powf(0.5);
         if dist != 0.0 {
@@ -33,7 +21,7 @@ impl Shar{
         }
     }
 
-    fn update(&mut self, time: f64){
+    pub fn update(&mut self, time: f64){
         self.velx += time*self.accx;
         self.vely += time*self.accy;
         self.x += time*self.velx;
@@ -41,24 +29,28 @@ impl Shar{
         self.accx = 0.0;
         self.accy = 0.0;    
     }
-
+    
     pub fn show(&self) -> (f64, f64, f64, f64, f64, f64 , f64){
         (self.massa, self.x, self.y, self.velx, self.vely, self.accy, self.accy)
     }
-
-
-
+    
 }
 
+
+
+
 pub fn run(){
-    let sun = &mut Shar::new(20.0, 400.0, 400.0, 0.0, 0.0, 0.0, 0.0);
-    let luna = Shar::new(20.0, 410.0, 420.0, 0.0, 0.0, 0.0, 0.0);
+    let mut luna = Shar{massa: 20.0, x: 410.0, y: 410.0, velx: 0.0, vely: 0.0, accx: 0.0, accy: 0.0};
+    let mut sun = Shar{massa: 20.0, x: 400.0, y: 400.0, velx: 0.0, vely: 0.0, accx: 0.0, accy: 0.0};
+    // let mut luna = Shar{20.0, 410.0, 420.0, 0.0, 0.0, 0.0, 0.0};
     println!("massa solnca {:?}", sun.show());
     println!("massa solnca {:?}", luna.show());
     sun.pull_by(&luna);
+    luna.pull_by(&sun);
     println!("massa solnca {:?}", sun.show());
     println!("massa solnca {:?}", luna.show());
     sun.update(1.0);
+    luna.update(1.0);
     println!("massa solnca {:?}", sun.show());
     println!("massa solnca {:?}", luna.show());
 }
@@ -80,7 +72,8 @@ pub fn run(){
 pub fn generate(times: i32) -> Vec<Shar> {
     let mut universe = Vec::new();
     for i in 1..times {
-        let mut sun = Shar::new(23.0, 400.0, 400.0, 0.0, 0.0, 0.0, 0.0);
+        let mut sun = Shar{massa: 20.0, x: 400.0, y: 400.0, velx: 0.0, vely: 0.0, accx: 0.0, accy: 0.0};
+        // let mut sun = Shar(23.0, 400.0, 400.0, 0.0, 0.0, 0.0, 0.0);
         // let sun = 43i32;
         universe.push(sun);
         println!("{:?}", i);
