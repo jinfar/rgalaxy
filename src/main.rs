8f4545f4a71da32292015623f99a89ecc40c2ed6
stas oc <jinfar@ymail.com>
2021-05-05 07:43:47 +0500

    sdelal izmenenie vselennix

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,20 @@
 #![allow(unused_mut)]
+
+// use shar::step;
 mod shar;
 
 
 fn main() {
     println!("Hello, world!");
-    // shar::run();
     let mut universe = shar::generate(4);
-    // println!("{:?}", w[1].show());
-    // let mut tmp = &w[1];
-    // let mut tmp2 = &w[2];
-    // w[1].pull_by(&w[2]);
-    // w[1].pull_by(tmp2);
-    for sun in universe {
-        println!("{:?}", sun.massa);
-    }
-    // println!("{:?}", w[1].massa);
-    // println!("{:?}", w[2].massa);
+    dbg!(universe[0].accx);
+    dbg!(universe[0].velx);
+    dbg!(universe[0].x);
+    dbg!(universe[0].y);
+    universe = shar::step(universe);
+    dbg!(universe[0].accx);
+    dbg!(universe[0].velx);
+    dbg!(universe[0].x);
+    dbg!(universe[0].y);
+    
 }
--- src/shar.rs
+++ src/shar.rs
@@ -1,82 +1,67 @@
 #![allow(dead_code)]
-#![allow(unused_mut)]
+// #![allow(unused_mut)]
 use rand::Rng;
 
+#[derive(Copy, Clone)]
 pub struct Shar {
     pub massa: f64,
-    x: f64,
-    y: f64,
-    velx: f64,
+    pub x: f64,
+    pub y: f64,
+    pub velx: f64,
     vely: f64,
-    accx: f64,
+    pub accx: f64,
     accy: f64,
 }
 
 impl Shar{
         
-    pub fn pull_by(&mut self, other: &Shar){
+    pub fn pull_by(&mut self, other: &Shar) -> Shar{
         let g = 1.0;
         let dist = ((self.x - other.x).powi(2)+(self.y - other.y).powi(2)).powf(0.5);
         if dist != 0.0 {
             self.accx += g*other.massa*(other.x - self.x)/dist/dist/dist;
             self.accy += g*other.massa*(other.y - self.y)/dist/dist/dist;
         }
+        *self
     }
 
-    pub fn update(&mut self, time: f64){
+    pub fn update(&mut self, time: f64) -> Shar{
         self.velx += time*self.accx;
         self.vely += time*self.accy;
         self.x += time*self.velx;
         self.y += time*self.vely;
         self.accx = 0.0;
         self.accy = 0.0;    
+        *self
     }
     
     pub fn show(&self) -> (f64, f64, f64, f64, f64, f64 , f64){
         (self.massa, self.x, self.y, self.velx, self.vely, self.accy, self.accy)
     }
-    
 }
 
-
-
-
-pub fn run(){
-    let mut luna = Shar{massa: 20.0, x: 410.0, y: 410.0, velx: 0.0, vely: 0.0, accx: 0.0, accy: 0.0};
-    let mut sun = Shar{massa: 20.0, x: 400.0, y: 400.0, velx: 0.0, vely: 0.0, accx: 0.0, accy: 0.0};
-    println!("massa solnca {:?}", sun.show());
-    println!("massa solnca {:?}", luna.show());
-    sun.pull_by(&luna);
-    luna.pull_by(&sun);
-    println!("massa solnca {:?}", sun.show());
-    println!("massa solnca {:?}", luna.show());
-    sun.update(1.0);
-    luna.update(1.0);
-    println!("massa solnca {:?}", sun.show());
-    println!("massa solnca {:?}", luna.show());
+pub fn step(universe: Vec<Shar>) -> Vec<Shar>{
+    let temp = &universe.clone();
+    let mut newverse: Vec<Shar> = vec![];
+    for mut sun in universe {
+        for luna in temp {
+            sun.pull_by(luna);
+        }
+        newverse.push(sun);
+    }
+    let mut newverse2: Vec<Shar> = vec![];
+    for mut sun in newverse {
+        sun.update(1.0);
+        newverse2.push(sun);
+    }    
+    return newverse2
 }
-
-// pub fn generate(times: i32) -> Vec<&'static mut Shar>{
-//     let Universe: Vec<&mut Shar>;
-//     for i in 1..times {
-//         let sun = &mut Shar::new(20.0, 400.0, 400.0, 0.0, 0.0, 0.0, 0.0);
-//         Universe.push(sun);
-//         println!("{:?}", i);
-//     }
-//     return Universe
-// }
-
-// fn fill () -> Shar{
-//     Shar{}
-// }
 
 pub fn generate(times: i32) -> Vec<Shar> {
     let mut universe = Vec::new();
     let mut rng = rand::thread_rng();
     let distr = rand_distr::Normal::new(0.0, 1.0).unwrap();
-    for i in 0..times {
-        // let massa = rng.gen_range(15.0..25.0);
-        // println!("{:?}", rng.sample(distr));
+    for _i in 0..times {
         let mut sun = Shar{
             massa: rng.gen_range(15.0..25.0), 
             x: 400.0 + 20.0 * rng.sample(distr), 
